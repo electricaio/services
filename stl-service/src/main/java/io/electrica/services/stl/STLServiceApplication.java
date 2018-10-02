780db701750d22bbb737ddb84a7dc5ee716bb040
Inder <inder.sabharwal@gmail.com>
2018-10-01 23:54:42 -0700

    stl-service module added similar to user-service. These are the only two modules that we need right now.

---
+++ stl-service/src/main/java/io/electrica/services/stl/STLServiceApplication.java
@@ -0,0 +1,13 @@
+package io.electrica.services.stl;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class STLServiceApplication {
+
+	public static void main(String... args) {
+		SpringApplication.run(STLServiceApplication.class, args);
+	}
+}
