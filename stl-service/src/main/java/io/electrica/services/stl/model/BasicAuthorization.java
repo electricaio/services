d6739a8cd49cdcdf33834123b3f1516a04be5b3b
Branko Gvoka <bane92ru@gmail.com>
2018-10-03 21:57:34 +0800

    Implement STL data model, add spring-data dependency

---
+++ stl-service/src/main/java/io/electrica/services/stl/model/BasicAuthorization.java
@@ -0,0 +1,96 @@
+package io.electrica.services.stl.model;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+
+@Entity
+@Table(name = "basic_authorizations")
+public class BasicAuthorization extends AbstractBaseEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @NotNull
+    @Size(max = 64)
+    @Column(name = "user_hash")
+    private String userHash;
+
+    @NotNull
+    @Size(max = 64)
+    @Column(name = "password_hash")
+    private String passwordHash;
+
+    @NotNull
+    @OneToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    @JoinColumn(name = "authorization_id")
+    private Authorization authorization;
+
+    public BasicAuthorization() {
+    }
+
+    public String getUserHash() {
+        return userHash;
+    }
+
+    public void setUserHash(String userHash) {
+        this.userHash = userHash;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public Authorization getAuthorization() {
+        return authorization;
+    }
+
+    public void setAuthorization(Authorization authorization) {
+        this.authorization = authorization;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        final BasicAuthorization other = (BasicAuthorization) obj;
+        if (this.getId() != null && other.getId() != null && !this.getId().equals(other.getId()))
+            return false;
+        if (this.userHash != null && other.userHash != null && !this.userHash.equals(other.userHash))
+            return false;
+        if (this.passwordHash != null && other.passwordHash != null && !this.passwordHash.equals(other.passwordHash))
+            return false;
+        if (!((this.authorization == null && other.authorization == null)
+                || (this.authorization != null && other.authorization != null && this.authorization.getId() == null && other.authorization.getId() == null)
+                || this.authorization.getId().equals(other.authorization.getId())))
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.getId() == null) ? 0 : this.getId().hashCode());
+        result = prime * result + ((this.userHash == null) ? 0 : this.userHash.hashCode());
+        result = prime * result + ((this.passwordHash == null) ? 0 : this.passwordHash.hashCode());
+        result = prime * result + ((this.authorization == null || this.authorization.getId() == null) ? 0 : this.authorization.getId().hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "BasicAuthorization[" + "this.id=" + this.getId() + ", this.authorization=" + (this.authorization == null ? this.authorization : this.authorization.getId()) + "]";
+    }
+
+}
---
+++ stl-service/src/main/java/io/electrica/services/stl/model/STLType.java
@@ -0,0 +1,60 @@
+package io.electrica.services.stl.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "stl_types")
+public class STLType extends AbstractBaseEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @NotNull
+    @Size(max = 255)
+    @Column(name = "name")
+    private String name;
+
+    public STLType() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        final STLType other = (STLType) obj;
+        if (this.getId() != null && other.getId() != null && !this.getId().equals(other.getId()))
+            return false;
+        if (this.name != null && other.name != null && !this.name.equals(other.name))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.getId() == null) ? 0 : this.getId().hashCode());
+        result = prime * result + ((this.name == null) ? 0 : this.name.hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "STLType[" + "this.id=" + this.getId() + ", this.name=" + this.name + "]";
+    }
+
+}
