96a42489ba67742bbb220998977d79c9c325c44c
aleks <aleksey.polukeev@gmail.com>
2018-10-03 22:46:43 +0400

    MAIN-43 - Set default settings for all services * common properties, settings * jackson config * some helper classes

---
+++ common/src/main/java/io/electrica/common/config/CommonModuleConfig.java
@@ -0,0 +1,19 @@
+package io.electrica.common.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import java.util.TimeZone;
+
+@Configuration
+@EnableConfigurationProperties
+@PropertySource("classpath:/common.properties")
+public class CommonModuleConfig {
+
+    static {
+        TimeZone utcTimeZone = TimeZone.getTimeZone("UTC");
+        TimeZone.setDefault(utcTimeZone);
+    }
+
+}
