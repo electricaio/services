d588ce5e3d563a58a902426a2f68beb240dde0e9
MunishSodhi <ermunishsodhi@gmail.com>
2018-10-05 18:06:21 +0530

    main-40 Changes as per review comments

--- user-service/src/main/java/io/electrica/UserServiceApplication.java
+++ user-service/src/main/java/io/electrica/UserServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Main class to start the User Service.
  *
  * Please see the {@link io.electrica.UserServiceApplication} class for true identity
- * @author Munish Sodhi
  *
  */
 @SpringBootApplication
--- user-service/src/test/java/io/electrica/user/rest/UserRestClientTest.java
+++ user-service/src/test/java/io/electrica/user/rest/UserRestClientTest.java
@@ -1,12 +1,18 @@
 package io.electrica.user.rest;
 
 import io.electrica.UserServiceApplication;
+import io.electrica.user.dto.CreateUserDto;
+import io.electrica.user.dto.UserDto;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.inject.Inject;
+
+import static org.junit.Assert.assertNotNull;
+
 /**
  *  UserRestClientTest to test rest client.
  *
@@ -25,8 +31,8 @@ public class UserRestClientTest {
 
     @Test
     public void createUser() {
-        /*ResponseEntity<UserDto> response = userRestClient.createUser(new CreateUserDto());
-        assertNotNull(response.getBody());*/
+        ResponseEntity<UserDto> response = userRestClient.createUser(new CreateUserDto());
+        assertNotNull(response.getBody());
 
     }
 
