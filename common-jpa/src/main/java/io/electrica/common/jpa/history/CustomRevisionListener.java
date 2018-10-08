e6dee20b691ccdae75a57f77ce2469452b3c082a
aleks <aleksey.polukeev@gmail.com>
2018-10-08 13:59:02 +0400

    MAIN-70 - Setup support of entity history, opt locks and common service layer - setup history with Hibernate Envers

---
+++ common-jpa/src/main/java/io/electrica/common/jpa/history/CustomRevisionListener.java
@@ -0,0 +1,17 @@
+package io.electrica.common.jpa.history;
+
+import io.electrica.common.context.Identity;
+import io.electrica.common.context.IdentityContextHolder;
+import org.hibernate.envers.RevisionListener;
+
+public class CustomRevisionListener implements RevisionListener {
+
+    @Override
+    public void newRevision(Object revisionEntity) {
+        CustomRevisionEntity entity = (CustomRevisionEntity) revisionEntity;
+        Identity identity = IdentityContextHolder.getInstance().getIdentity();
+        // case when non-rest request (context filter doesn't work)
+        int userId = identity != null ? identity.getUserId() : Identity.NOT_AUTHENTICATED_USER_ID;
+        entity.setUserId(userId);
+    }
+}
