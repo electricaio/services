d270eb25e23908a1a4f625f97be6ddb9ec2d0a8c
MunishSodhi <ermunishsodhi@gmail.com>
2018-10-10 09:34:58 +0530

    main-55 partial checkin

---
+++ user-service/src/main/java/io/electrica/user/repository/UserRoleRepository.java
@@ -0,0 +1,6 @@
+package io.electrica.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRoleRepository extends JpaRepository<UserRoleRepository,Long> {
+}
