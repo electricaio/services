e3937c75c945951705710e7f85fc41ead3cc42a8
MunishSodhi <ermunishsodhi@gmail.com>
2018-10-12 07:42:10 +0530

    main-55 organize import

--- user-service/src/main/java/io/electrica/user/service/UserService.java
+++ user-service/src/main/java/io/electrica/user/service/UserService.java
@@ -17,7 +17,6 @@ import javax.inject.Inject;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Optional;
 
 /**
  * User Service implementation class for managing users.
