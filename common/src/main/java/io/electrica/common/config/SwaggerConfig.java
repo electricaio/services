271dd26a9656caac97a370bfc4b4cb0ea3e68cc3
aleks <aleksey.polukeev@gmail.com>
2018-10-29 17:59:35 +0400

    MAIN-87 - Add Annotations to controllers to make Swagger files more specifics. - add swagger description - support OAuth2 authorization scheme - add oauth route to gateway

--- common/src/main/java/io/electrica/common/config/SwaggerConfig.java
+++ common/src/main/java/io/electrica/common/config/SwaggerConfig.java
@@ -1,23 +1,103 @@
 package io.electrica.common.config;
 
+import io.electrica.common.EnvironmentType;
+import io.electrica.common.helper.AuthorityConstants;
+import io.electrica.common.helper.RequestHelper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.RequestMethod;
+import springfox.documentation.builders.OAuthBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.inject.Inject;
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
+    private static final String PASSWORD_SCHEME = "Electrica OAuth2 Password Client";
+
+    @Inject
+    @Value("${common.service.version}")
+    private String version;
+
+    @Inject
+    private EnvironmentType environmentType;
+
     @Bean
     public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .apis(RequestHandlerSelectors.any())
+                .paths(input -> input != null &&
+                        !RequestHelper.isPrivateApi(input) &&
+                        !RequestHelper.isAuthPath(input) &&
+                        !RequestHelper.isErrorPath(input)
+                )
+                .build()
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(RequestMethod.GET, Arrays.asList(
+                        new ResponseMessageBuilder()
+                                .code(500)
+                                .message("500 message")
+                                //.responseModel(new ModelRef("Error"))
+                                .build(),
+                        new ResponseMessageBuilder()
+                                .code(403)
+                                .message("Forbidden!")
+                                .build()
+                ))
+                .securitySchemes(Collections.singletonList(securityScheme()))
+                .securityContexts(Collections.singletonList(securityContext()));
+    }
+
+    private SecurityScheme securityScheme() {
+        GrantType grantType = new ResourceOwnerPasswordCredentialsGrant("/oauth/token");
+        return new OAuthBuilder().name(PASSWORD_SCHEME)
+                .grantTypes(Collections.singletonList(grantType))
+                .scopes(Arrays.asList(scopes()))
                 .build();
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(Collections.singletonList(new SecurityReference(PASSWORD_SCHEME, scopes())))
+                .forPaths(RequestHelper::isSecuredResource)
+                .build();
+    }
+
+    private AuthorizationScope[] scopes() {
+        return new AuthorizationScope[]{
+                new AuthorizationScope(AuthorityConstants.CREATE_SCOPE, "for create operations"),
+                new AuthorizationScope(AuthorityConstants.READ_SCOPE, "for read operations"),
+                new AuthorizationScope(AuthorityConstants.UPDATE_SCOPE, "for update operations"),
+                new AuthorizationScope(AuthorityConstants.DELETE_SCOPE, "for delete operations"),
+                new AuthorizationScope(AuthorityConstants.DO_SCOPE, "for change state operations"),
+                new AuthorizationScope(AuthorityConstants.INVOKE_SCOPE, "for SDK invoke operations")
+        };
+    }
+
+    private ApiInfo apiInfo() {
+        String versionDescription = environmentType.isSafe() ? "-" + environmentType : "";
+        return new ApiInfo(
+                "Electrica REST API",
+                "Documentation for Electrica public REST API services.",
+                version + versionDescription,
+                "https://electrica.io/terms-of-service",
+                new Contact("Support", "https://electrica.io/support", "support@electrica.io"),
+                null,
+                null,
+                Collections.emptyList()
+        );
     }
 }
