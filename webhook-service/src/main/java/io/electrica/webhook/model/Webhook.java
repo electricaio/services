1be5df51c444d5a02641d818d7f67bc95884a630
MunishSodhi <ermunishsodhi@gmail.com>
2018-11-22 14:10:41 +0530

    MAIN-199 - Create DB structure for webhook service and related changes
    - Changes as per review comments

--- webhook-service/src/main/java/io/electrica/webhook/model/Webhook.java
+++ webhook-service/src/main/java/io/electrica/webhook/model/Webhook.java
@@ -1,16 +1,12 @@
 package io.electrica.webhook.model;
 
-import io.electrica.common.jpa.model.AbstractEntity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
@@ -26,15 +22,16 @@ import java.util.UUID;
 @Table(name = "webhooks",
         uniqueConstraints = @UniqueConstraint(columnNames = {"name", "connection_id"})
 )
-public class Webhook extends AbstractEntity {
+public class Webhook {
+
+    @Id
+    @Column(unique = true)
+    private UUID token;
 
     @NotNull
     @Size(max = 255)
     @Column(name = "name")
     private String name;
-
-    @Column(unique = true)
-    private UUID token;
 
     @NotNull
     @Column(name = "connection_id")
