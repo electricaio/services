e06c89ff2c5506b8f7f7860d391be322daaaae94
MunishSodhi <ermunishsodhi@gmail.com>
2018-12-12 12:49:33 +0530

    MAIN-228 - Integration Test for Slack V1
    -- Task related changes

---
+++ it-service/src/test/java/io/electrica/it/integration/slack/v1/SlackV1Test.java
@@ -0,0 +1,41 @@
+package io.electrica.it.integration.slack.v1;
+
+import io.electrica.it.BaseIT;
+import io.electrica.sdk.java.core.Electrica;
+import io.electrica.sdk.java.core.exception.IntegrationException;
+import io.electrica.sdk.java.core.http.impl.HttpModuleImpl;
+import io.electrica.sdk.java.slack.channel.v1.SlackChannelV1Manager;
+import io.electrica.user.dto.AccessKeyDto;
+import io.electrica.user.dto.FullAccessKeyDto;
+import io.electrica.user.dto.UserDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.testng.annotations.Test;
+
+import static io.electrica.it.util.ItServiceConstants.FILL_DATA_GROUP;
+import static org.testng.Assert.assertTrue;
+
+public class SlackV1Test extends BaseIT {
+
+    @Value("${feign.invokerService.url}")
+    private String invokerServiceUrl;
+
+    @Value("${it-service.slack.v1.channel}")
+    private String slackChannel;
+
+    @Test(dependsOnGroups = {FILL_DATA_GROUP})
+    public void testSlackV1() throws IntegrationException {
+
+        UserDto user = contextHolder.getUsers().get(0);
+        contextHolder.setContextForUser(user.getEmail());
+        AccessKeyDto accessKeyDto = accessKeyClient.findAllNonArchivedByUser(user.getId()).getBody().get(0);
+        FullAccessKeyDto fullAccessKeyDto = accessKeyClient.getAccessKey(accessKeyDto.getId()).getBody();
+
+        Electrica instance = Electrica.instance(() -> new HttpModuleImpl("http://" + invokerServiceUrl),
+                fullAccessKeyDto.getKey());
+        SlackChannelV1Manager channelManager = new SlackChannelV1Manager(instance);
+        boolean result = channelManager.sendMessage(slackChannel, "Hi.. This is Integration Service Test");
+        assertTrue(result);
+
+    }
+}
+
