d7bc3079f700bd1622472d55566e538ec7ffd6ff
MunishSodhi <ermunishsodhi@gmail.com>
2018-12-13 13:28:31 +0530

    MAIN-228 - Integration Test for Slack V1
    -- Task related changes

--- it-service/src/test/java/io/electrica/it/BaseIT.java
+++ it-service/src/test/java/io/electrica/it/BaseIT.java
@@ -1,6 +1,7 @@
 package io.electrica.it;
 
 import io.electrica.ItServiceApplication;
+import io.electrica.common.config.EnvironmentTypeConfig;
 import io.electrica.connector.hub.feign.AuthorizationClient;
 import io.electrica.connector.hub.feign.ConnectionClient;
 import io.electrica.connector.hub.feign.ConnectorClient;
@@ -10,6 +11,7 @@ import io.electrica.user.feign.AccessKeyClient;
 import io.electrica.user.feign.OrganizationClient;
 import io.electrica.user.feign.UserClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
 
@@ -18,7 +20,8 @@ import javax.inject.Inject;
 import static io.electrica.it.util.ItServiceConstants.INIT_GROUP;
 
 @SpringBootTest(classes = ItServiceApplication.class)
-public class BaseIT extends AbstractTestNGSpringContextTests {
+@ActiveProfiles(EnvironmentTypeConfig.TEST_ENV_PROFILE_NAME)
+public abstract class BaseIT extends AbstractTestNGSpringContextTests {
 
     public static final String ORG_HACKER_RANK = "HackerRank";
     public static final String ORG_TOP_CODER = "TopCoder";
