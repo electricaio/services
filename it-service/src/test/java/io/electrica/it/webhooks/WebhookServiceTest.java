7e5e33476975f83c5c6d995f47b10ce902057dc3
MunishSodhi <ermunishsodhi@gmail.com>
2019-01-06 23:39:10 +0530

    MAIN-230 - Integration tests: Webhook
    -- Add end to end test for webhook

--- it-service/src/test/java/io/electrica/it/webhooks/WebhookServiceTest.java
+++ it-service/src/test/java/io/electrica/it/webhooks/WebhookServiceTest.java
@@ -1,22 +1,37 @@
 package io.electrica.it.webhooks;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.electrica.common.security.RoleType;
 import io.electrica.connector.hub.dto.ConnectionDto;
 import io.electrica.connector.hub.dto.ConnectorDto;
 import io.electrica.it.BaseIT;
+import io.electrica.it.auth.TokenDetails;
+import io.electrica.sdk.java8.api.Connection;
+import io.electrica.sdk.java8.api.Connector;
+import io.electrica.sdk.java8.api.Electrica;
+import io.electrica.sdk.java8.api.MessageListener;
+import io.electrica.sdk.java8.api.http.Message;
+import io.electrica.sdk.java8.core.SingleInstanceHttpModule;
 import io.electrica.user.dto.AccessKeyDto;
+import io.electrica.user.dto.FullAccessKeyDto;
 import io.electrica.user.dto.UserDto;
 import io.electrica.webhook.dto.ConnectionCreateWebhookDto;
 import io.electrica.webhook.dto.ConnectionWebhookDto;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import lombok.*;
+import org.junit.Assert;
+import org.junit.jupiter.api.*;
 
+import javax.inject.Inject;
 import java.util.Collections;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(value = TestInstance.Lifecycle.PER_CLASS)
 public class WebhookServiceTest extends BaseIT {
@@ -25,15 +40,57 @@ public class WebhookServiceTest extends BaseIT {
     private static final Map<String, String> TEST_WEBHOOK_PROPERTIES = Collections.singletonMap("a", "b");
     private UserDto user;
 
+    private static final String ECHO_CONNECTOR_ERN = "ern://echo:test:1";
+
+    @Inject
+    ObjectMapper mapper;
+
+    private Connection connection;
+    private Electrica instance;
+    private WebhookMessageHandler handler;
+    private ConnectionWebhookDto webhookDto;
+
     @BeforeAll
     public void setUp() {
         init();
         user = createUser(ORG_HACKER_RANK, RoleType.OrgUser);
-        ConnectorDto connectorDto = createConnector(WEBHOOK_PREFIX + getCurrTimeAsString(), "1");
         contextHolder.setContextForUser(user.getEmail());
-        AccessKeyDto accessKeyDto = createAccessKey(user.getId(), WEBHOOK_PREFIX + getCurrTimeAsString());
-        createConnection("webhookTest", connectorDto, accessKeyDto.getId());
+        createWebhookConnection();
     }
+
+    private void createWebhookConnection() {
+        AccessKeyDto accessKey = createAccessKey(user.getId(), WEBHOOK_PREFIX + getCurrTimeAsString());
+        ConnectorDto connectorDto = getEchoConnector();
+        ConnectionDto connectionDto = createConnection(WEBHOOK_PREFIX + getCurrTimeAsString(), connectorDto,
+                accessKey.getId());
+        webhookDto = createAndSaveWebhook(connectionDto.getId(), accessKey.getId());
+        FullAccessKeyDto fullAccessKeyDto = accessKeyClient.getAccessKey(accessKey.getId()).getBody();
+        instance = Electrica.instance(new SingleInstanceHttpModule(invokerServiceUrl), fullAccessKeyDto.getKey());
+        Connector connector = instance.connector(ECHO_CONNECTOR_ERN);
+        connection = connector.connection(connectionDto.getName());
+        handler = new WebhookMessageHandler();
+        connection.addMessageListener(x -> true, handler);
+    }
+
+    @BeforeEach
+    public void setContext() {
+        contextHolder.setContextForUser(user.getEmail());
+    }
+
+    @AfterAll
+    void tearDown() {
+        try {
+            connection.close();
+        } catch (Exception e) {
+            Assert.fail("Exception while closing electrica instance:" + e.getMessage());
+        }
+        try {
+            instance.close();
+        } catch (Exception e) {
+            Assert.fail("Exception while closing connection:" + e.getMessage());
+        }
+    }
+
 
     @Test
     public void testAddWebhook() {
@@ -61,11 +118,15 @@ public class WebhookServiceTest extends BaseIT {
     }
 
     private ConnectionWebhookDto createAndSaveWebhook(Long accessKeyId) {
-        ConnectionDto connectionDtos = connectionClient.findAllByAccessKeyId(accessKeyId).getBody().get(0);
-        assertNotNull(connectionDtos);
+        ConnectionDto connectionDto = connectionClient.findAllByAccessKeyId(accessKeyId).getBody().get(0);
+        assertNotNull(connectionDto);
+        return createAndSaveWebhook(connectionDto.getAccessKeyId(), accessKeyId);
+    }
+
+    private ConnectionWebhookDto createAndSaveWebhook(Long connectionId, Long accessKeyId) {
         ConnectionCreateWebhookDto webhookDto = new ConnectionCreateWebhookDto();
         webhookDto.setAccessKeyId(accessKeyId);
-        webhookDto.setConnectionId(connectionDtos.getId());
+        webhookDto.setConnectionId(connectionId);
         webhookDto.setName(WEBHOOK_PREFIX + getCurrTimeInMillSeconds());
         webhookDto.setProperties(TEST_WEBHOOK_PROPERTIES);
         ConnectionWebhookDto response = webhookClient.createConnection(webhookDto).getBody();
@@ -76,5 +137,59 @@ public class WebhookServiceTest extends BaseIT {
         assertEquals(webhookDto.getConnectionId(), response.getConnectionId());
         return response;
     }
+
+    @Test
+    public void testWebhookInvoke() {
+        TokenDetails td = new TokenDetails();
+        td.setAccessToken(instance.getAccessKey());
+        contextHolder.setContext(td);
+        JsonNode input = mapper.createObjectNode().put("a", "a");
+        Object out = webhookClient.invoke2(webhookDto.getId(), input, 60000).getBody();
+        assertNotNull(out);
+    }
+
+    @Test
+    public void testWebhookSubmit() {
+        TokenDetails td = new TokenDetails();
+        td.setAccessToken(instance.getAccessKey());
+        contextHolder.setContext(td);
+        JsonNode input = mapper.createObjectNode().put("a", "a");
+        webhookClient.submit(webhookDto.getId(), input).getBody();
+        assertTrue(handler.awaitResponse());
+    }
+
+    private ConnectorDto getEchoConnector() {
+        return connectorClient.findAll().getBody().stream()
+                .filter(c -> Objects.equals(c.getErn(), ECHO_CONNECTOR_ERN))
+                .findFirst().get();
+    }
+
+    private class WebhookMessageHandler implements MessageListener {
+
+        private final BlockingQueue<Boolean> queue = new ArrayBlockingQueue<>(1);
+
+        @Override
+        @SneakyThrows
+        public Optional<Object> onMessage(Message message) {
+            Result result = new Result("Test");
+            JsonNode out = mapper.convertValue(result, JsonNode.class);
+            queue.add(Boolean.TRUE);
+            return Optional.of(out);
+        }
+
+        @SneakyThrows
+        private Boolean awaitResponse() {
+            return queue.poll(60, TimeUnit.SECONDS);
+        }
+    }
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @Setter
+    static class Result {
+        public String result;
+    }
+
 
 }
--- webhook-api/src/main/java/io/electrica/webhook/feign/WebhookClient.java
+++ webhook-api/src/main/java/io/electrica/webhook/feign/WebhookClient.java
@@ -1,8 +1,25 @@
 package io.electrica.webhook.feign;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import io.electrica.webhook.rest.WebhookController;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.UUID;
+
+import static io.electrica.common.rest.PathConstants.V1;
 
 @FeignClient(name = "webhookClient", url = "${feign.webhookService.url}")
 public interface WebhookClient extends WebhookController {
+
+     @PostMapping(V1 + "/webhooks/{webhookId}/invoke")
+     ResponseEntity<Object> invoke2(
+            @PathVariable("webhookId") UUID webhookId,
+            @RequestBody JsonNode payload,
+            @RequestParam(name = "timeout", required = false, defaultValue = "60000") long timeout
+    );
 }
