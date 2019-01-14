36b0a49a7d055e8fe398bd5c29efd8e2f3652ed8
MunishSodhi <ermunishsodhi@gmail.com>
2019-01-14 15:36:48 +0530

    MAIN-250 - Create Integration Test for AccessKey Client for user service
    -- changes as per review comments

--- it-service/src/test/java/io/electrica/it/user/AccessKeyServiceTest.java
+++ it-service/src/test/java/io/electrica/it/user/AccessKeyServiceTest.java
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,29 +21,28 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AccessKeyServiceTest extends BaseIT {
 
     private UserDto user;
+    private AccessKeyDto accessKeyDto;
 
     @BeforeAll
     public void setUp() {
         init();
         user = createUser(ORG_HACKER_RANK, RoleType.OrgUser);
         contextHolder.setContextForUser(user.getEmail());
-        createAccessKey(user.getId(), user.getId() + getCurrTimeAsString());
+        accessKeyDto = createAccessKey(user.getId(), user.getId() + getCurrTimeAsString());
     }
 
     @Test
     public void testFindAllNonArchivedByUser() {
         List<AccessKeyDto> accessKeyDtoList = accessKeyClient.findAllNonArchivedByUser(user.getId()).getBody();
-        assertTrue(accessKeyDtoList.size() > 0);
+        assertEquals(1, accessKeyDtoList.size());
     }
 
     @Test
     public void testGetAccessKey() {
-        AccessKeyDto accessKeyDto = accessKeyClient.findAllNonArchivedByUser(user.getId()).getBody().get(0);
         FullAccessKeyDto fullAccessKeyDto = accessKeyClient.getAccessKey(accessKeyDto.getId()).getBody();
         assertNotNull(fullAccessKeyDto.getJti());
         assertNotNull(fullAccessKeyDto.getKey());
-        assertEquals(accessKeyDto.getId(), fullAccessKeyDto.getId());
-        assertEquals(accessKeyDto.getRevisionVersion(), fullAccessKeyDto.getRevisionVersion());
+        compareAccessKey(accessKeyDto, fullAccessKeyDto);
     }
 
     @Test
@@ -50,9 +51,9 @@ public class AccessKeyServiceTest extends BaseIT {
         FullAccessKeyDto fullAccessKeyDto = accessKeyClient.getAccessKey(accessKeyDto.getId()).getBody();
         AccessKeyDto newAccessKeyDto = accessKeyClient.refreshAccessKey(accessKeyDto.getId()).getBody();
         FullAccessKeyDto newFullAccessKeyDto = accessKeyClient.getAccessKey(accessKeyDto.getId()).getBody();
-        assertEquals(accessKeyDto.getId(), newAccessKeyDto.getId());
         assertNotEquals(fullAccessKeyDto.getJti(), newFullAccessKeyDto.getJti());
         assertNotEquals(fullAccessKeyDto.getKey(), newFullAccessKeyDto.getKey());
+        compareAccessKey(accessKeyDto, newAccessKeyDto);
     }
 
     @Test
@@ -60,11 +61,23 @@ public class AccessKeyServiceTest extends BaseIT {
         CreateAccessKeyDto accessKeyDto = new CreateAccessKeyDto();
         accessKeyDto.setName("Test");
         accessKeyDto.setUserId(user.getId());
-        AccessKeyDto accessKeyDto1 = accessKeyClient.createAccessKey(accessKeyDto).getBody();
+        int accessKeysCount = accessKeyClient.findAllNonArchivedByUser(user.getId()).getBody().size();
+        AccessKeyDto newAccessKey = accessKeyClient.createAccessKey(accessKeyDto).getBody();
+        int accessKeysCountAfterInsert = accessKeyClient.findAllNonArchivedByUser(user.getId()).getBody().size();
+        assertEquals(accessKeysCount + 1, accessKeysCountAfterInsert);
+        accessKeyClient.deleteAccessKey(newAccessKey.getId());
         List<AccessKeyDto> accessKeyDtoList = accessKeyClient.findAllNonArchivedByUser(user.getId()).getBody();
-        assertTrue(accessKeyDtoList.size() > 0);
-        accessKeyClient.deleteAccessKey(accessKeyDto1.getId());
-        List<AccessKeyDto> newAccessKeyDtoList = accessKeyClient.findAllNonArchivedByUser(user.getId()).getBody();
-        assertEquals(accessKeyDtoList.size() - 1, newAccessKeyDtoList.size());
+        Optional<AccessKeyDto> optionalAccessKeyDto = accessKeyDtoList.stream()
+                .filter(a -> Objects.equals(a.getId(), newAccessKey.getId()))
+                .findAny();
+        assertFalse(optionalAccessKeyDto.isPresent());
+        assertEquals(accessKeysCount, accessKeyDtoList.size());
+    }
+
+    private void compareAccessKey(AccessKeyDto accessKeyDto, AccessKeyDto newAccessKeyDto) {
+        assertEquals(accessKeyDto.getName(), newAccessKeyDto.getName());
+        assertEquals(accessKeyDto.getUserId(), newAccessKeyDto.getUserId());
+        assertEquals(accessKeyDto.getId(), newAccessKeyDto.getId());
+        assertEquals(accessKeyDto.getCreatedAt(), newAccessKeyDto.getCreatedAt());
     }
 }
