039f91efc04df1a17f9e425d4d7a17a30ccf8ad7
MunishSodhi <ermunishsodhi@gmail.com>
2019-01-18 09:48:35 +0530

    MAIN-299 - BrassRing Connector Migration
    -- Changes as per review comments

--- connector-hub-service/src/main/java/io/electrica/migration/common/V0_0_1_040__Seed_BrassRing_Applications_v1_connector.java
+++ connector-hub-service/src/main/java/io/electrica/migration/common/V0_0_1_040__Seed_BrassRing_Applications_v1_connector.java
@@ -11,7 +11,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import java.util.HashMap;
 import java.util.Map;
 
-public class V0_0_1_040__Seed_BrassRing_v1_connector implements SpringJdbcMigration {
+public class V0_0_1_040__Seed_BrassRing_Applications_v1_connector implements SpringJdbcMigration {
 
     private static final Map<String, String> PROPERTIES = new HashMap<String, String>() {{
         put("http-client.max-idle-connections", "10");
@@ -24,15 +24,15 @@ public class V0_0_1_040__Seed_BrassRing_v1_connector implements SpringJdbcMigrat
         CreateConnectorDto connector = new CreateConnectorDto(
                 null,
                 AuthorizationType.Token,
-                "BrassRing Applications",
-                "BrassRing Applications Connector",
+                "BrassRing Connector v1",
+                "Bi-directional BrassRing Connector for testing candidates using third-party SaaS providers",
                 "brassring",
                 "applications",
                 "1",
                 "https://www.electrica.io",
                 "https://www.ibm.com/in-en/talent-management",
                 "https://www.electrica.io",
-                "https://developer.ibm.com/answers/storage/attachments/19160-brassring-logo.png",
+                "https://images.electrica.io/brassring-logo.png",
                 PROPERTIES
         );
         MigrationUtils.saveConnector(context, connector, "Talent");
