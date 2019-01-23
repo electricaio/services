e83a386ead6f9b61f568f62fc38b994384435a2d
aleks <aleksey.polukeev@gmail.com>
2019-01-23 20:53:39 +0400

    Fix stage stand update - fix typo

--- deploy/build-push-stage.sh
+++ deploy/build-push-stage.sh
@@ -3,7 +3,7 @@
 echo ========================================= Started at $(date) =========================================
 
 cd /home/ubuntu/services && \
-  ./gradlew clean assembly && \
+  ./gradlew clean assemble && \
   sh deploy/docker/build.stage.sh && \
   export TAG=stage && \
   sh deploy/teamcity/push-docker.sh
