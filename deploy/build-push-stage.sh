b9ba5c51bc10b6996825266734bec104c344f02f
aleks <aleksey.polukeev@gmail.com>
2019-01-23 20:52:11 +0400

    Fix stage stand update 2

--- deploy/build-push-stage.sh
+++ deploy/build-push-stage.sh
@@ -3,6 +3,7 @@
 echo ========================================= Started at $(date) =========================================
 
 cd /home/ubuntu/services && \
+  ./gradlew clean assembly && \
   sh deploy/docker/build.stage.sh && \
   export TAG=stage && \
   sh deploy/teamcity/push-docker.sh
