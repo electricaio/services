1cf78484b5ae05d0549a6e29c36a8bf791a13590
aleks <aleksey.polukeev@gmail.com>
2019-01-28 07:45:16 +0400

    Update dependencies during the build

--- deploy/build-push-stage.sh
+++ deploy/build-push-stage.sh
@@ -3,7 +3,7 @@
 echo ========================================= Started at $(date) =========================================
 
 cd /home/ubuntu/services && \
-  ./gradlew clean assemble && \
+  ./gradlew clean assemble --refresh-dependencies && \
   sh deploy/docker/build.stage.sh && \
   export TAG=stage && \
   sh deploy/teamcity/push-docker.sh
