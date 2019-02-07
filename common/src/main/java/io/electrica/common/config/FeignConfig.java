49c9687a3b7d46cab82ccd4afb3a9e28107de5fc
petrovskiyroman <roman.petrovskiy@electrica.io>
2019-02-07 05:52:07 +0400

    Integration test for ConnectionEstablished and ConnectionClosed events (#216)

--- common/src/main/java/io/electrica/common/config/FeignConfig.java
+++ common/src/main/java/io/electrica/common/config/FeignConfig.java
@@ -1,9 +1,26 @@
 package io.electrica.common.config;
 
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignFormatterRegistrar;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 
 @Configuration
 @EnableFeignClients(basePackages = {"io.electrica"})
 public class FeignConfig {
+
+    @Bean
+    public FeignFormatterRegistrar localDateFeignFormatterRegistrar() {
+        return new FeignFormatterRegistrar() {
+            @Override
+            public void registerFormatters(FormatterRegistry formatterRegistry) {
+                DateTimeFormatterRegistrar registrar = new DateTimeFormatterRegistrar();
+                registrar.setUseIsoFormat(true);
+                registrar.registerFormatters(formatterRegistry);
+            }
+        };
+    }
+
 }
