f2c2d5fbd4a34c518c37489f5c705022274b43a7
aleks <aleksey.polukeev@gmail.com>
2019-02-12 15:18:27 +0400

    MAIN-336 - HELM chart for Connector service - implement build script - move invoker-service to electrica

---
+++ deploy/helm/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+helm_dir=deploy/helm
+if [ $HELM_DIR ]; then
+  helm_dir="$HELM_DIR"
+fi
+
+electrica_charts_dir=${helm_dir}/electrica/charts
+
+echo && \
+echo --------------------- Linting and packaging common --------------------- && \
+helm lint --strict ${helm_dir}/common && helm package ${helm_dir}/common && \
+\
+echo && \
+echo --------------------- Linting and packaging invoker-service --------------------- && \
+cp -v common-*.tgz ${electrica_charts_dir}/invoker-service/charts && \
+  helm lint ${electrica_charts_dir}/invoker-service && \
+  helm package ${electrica_charts_dir}/invoker-service && \
+\
+echo && \
+echo --------------------- Linting and packaging electrica --------------------- && \
+helm lint ${helm_dir}/electrica && helm package ${helm_dir}/electrica
